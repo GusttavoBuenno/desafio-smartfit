ceb96a8e6c463637dc55784621857aac1d31bc34
Gustavo <gustavobueno05@outlook.combr>
2023-11-01 14:53:45 -0300

    feat: add filter open units

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -2,28 +2,38 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { GetUnitsService } from 'src/app/services/get-units.service';
 
+
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
   styleUrls: ['./forms.component.scss']
 })
 export class FormsComponent implements OnInit {
-  results = [];
+  results: Location[]  = [];
+  filteredResults: Location[] = [];
   formGroup!: FormGroup;
 
   constructor(private formBuilder: FormBuilder, private unitService: GetUnitsService){}
 
-  ngOnInit(): void{
-    this.unitService.getAllUnits().subscribe(data => console.log(data));
+  ngOnInit(): void{ 
     this.formGroup = this.formBuilder.group({
       hour: '',
-      showClosed: false
+      showClosed: true
     })
+
+    this.unitService.getAllUnits().subscribe(data => {
+     this.results = data.locations;
+     this.filteredResults = data.locations;
+    });
   }
 
   onSubmit(): void{
-    console.log(this.formGroup.value)
-  }
+   if(!this.formGroup.value.showClosed) {
+    this.filteredResults = this.results.filter(location => location.opened === true)
+   } else{
+    this.filteredResults = this.results;
+   }
+  } 
 
   onClean(): void{
     this.formGroup.reset();
